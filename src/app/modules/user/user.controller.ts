38aa5c2668441257dcb337535536cc8090940f45
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-17 14:01:54 +0600

    error handle

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,18 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createCustomer = async (req: Request, res: Response) => {
-    const data = req.body
-    const result = await userService.createCustomer(data)
-    res.status(200).send(result)
+    try {
+        const data = req.body
+        const result = await userService.createCustomer(data)
+        res.status(200).send(result)
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            data: null,
+            error: err
+        })
+    }
 }
 
 export const userController = { createCustomer }
